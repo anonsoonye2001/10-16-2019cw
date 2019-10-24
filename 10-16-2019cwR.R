ec3d079544a7e81f8ecd01cec0f73f7964d18a48
chukwunonso chukwudozie <anonsoonye2001@yahoo.co.uk>
2019-10-23 23:48:44 -0500

    k

--- 10-16-2019cwR.R
+++ 10-16-2019cwR.R
@@ -1,68 +1,39 @@
-library(tidyverse)
 install.packages("plyr")
+library(tidyverse)
 library(plyr)
 
 load("fish_data.Rdata")
 
+#test subset to make sure all functions inside the "ddply" umbrella function work
 d=fish[fish$depth_fac=="Deep",]
 
 #ddply
 
-nd=ddply(.data=fish,.variables="depth_fac",function(x){
-  
-z=unique(d$depth_fac)
-#ifelse(test = z == "Deep", yes = 50, no =25)
-
-depth_condition=function(y){
-  if(y=="Deep")
-    q<-50
-  else if(z=="Mid")
-    q<-25
-  else
-    q<-15
-}
-x$depth_z=depth_condition(y=z)
-return(x)
-},.inform = T,.progress = "test")
-
-
-nd = ddply(.data = fish, .variables = "depth_fac", function(x){
-  
-  z = unique(x$depth_fac)
-  
-  #ifelse(test = z == "Deep", yes = 50, no =25)
-  
-  depth_condition = function(y){
-    if(y=="Deep")
-      q = 50
-    else if(y=="Mid")
-      q = 25
+nd<- ddply(.data=fish, .variables="depth_fac",function(x){
+  z<-unique(x$depth_fac)
+  #ifelse(test=z=="Deep",yes=50,no=25)
+  depth_condition<-function(y){
+    if(z=="Deep")
+      q<-50
+    else if(z=="Mid")
+      q<-25
     else
-      q = 15
+      q<-15
   }
   
-  x$depth_z = depth_condition(y = z)
-  
+  x$depth_z<-depth_condition(y=z)
   return(x)
-  
-}, .inform = T, .progress = "test")
- 
+}, .inform=T,.progress = "text")
 
 #Test if depth_condition works
-test = depth_condition(y = "Mid")
+
+test=depth_condition(y="Mid")
 test
+
 
 #Test return of correct values
 unique(nd$depth_z)
 print(depth_condition(y="Deep"))
-
-
-
-
-
-nd=ddply(.data=fish,.variables="depth_fac",function(x){
-  
-}
 
 d$depth_m
 
@@ -70,36 +41,9 @@ d$depth_m
 
 #dlpy-------------
 
-#list all the physical dat files in a given directory
+#list all the physical data files in a given directory
 batch_data=list.files(paste0("batch_data"),full=TRUE,pattern="ISIIS")
 
-phy=adply(.data=batch_data,.margins=1,function(file){
-#read the data  
-d=read.table(batch_data[1],sep="\t",skip=10,header=TRUE,fileEncoding = "ISO-8859-1",
-             stringsAsFactors = FALSE,quote="\"",check.names = FALSE,encoding ="UTF-8",
-             na.strings = "9999.99")
-#clean names
-head=names(d)
-head=str_replace(head, "\\(.*\\)","")
-head=str_trim(head)
-head=make.names(head)
-head=tolower(head)
-head=str_replace(head,fixed(".."),".")
-#assign names
-names(d=head)
-#create a proper date + time format
-date=scan(batch_data[1], what="character",skip=1,nlines = 1,quiet=TRUE)
-d$date=date[2]
-d$dateTime=str_c(d$date,d$time,sep="")
-d$dateTime=as.POSIXct(strptime(x=d$dateTime,
-                               format="%m/%d/%y %H:%M:%OS", tz="America/New_York"))
-
-return(d)
-},.inform = T, .progress="text")
-
-
-# list all the physical data files in a given directory
-batch_data <- list.files(paste0("batch_data"), full=TRUE, pattern = "ISIIS")
 
 phy <- adply(batch_data, 1, function(file) {
   
@@ -129,8 +73,9 @@ phy <- adply(batch_data, 1, function(file) {
   
   return(d)
   
-}, .progress="text")
+}, .inform=T,.progress="text")
 
+#23Oct2019
 
 # for loop
 #1) starting with temperature conversion
@@ -154,7 +99,7 @@ for(i in 1:10){
 
 rm(t)
 
-#2) combine with if else, set afloor for temp
+#2) combine with if else, set a floor for temp
 
 t<-data.frame(f_deg=seq(0,100,1))
 t
@@ -233,13 +178,14 @@ patch.df=do.cal()
 
 ## plotting
 #using ggplot 2
+
 install.packages("tidyverse")
 library(tidyverse)
 library(ggplot2)
 
 load("fish_data.Rdata")
 
-# Non "ggplot2" 
+# Non "ggplot2" plotting function
 fish.deep=fish[fish$deep_fac=="Deep"]
 
 plot(x=fish.deep$parcel.start.lon,
